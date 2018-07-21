f3dbcdb2c514cf78ac9403286b2d9c7111548ab2
Joana Palero <joana.palero24@gmail.com>
2018-07-21 22:11:55 +0000

    initial commit

---
+++ top_artists/lib/top_artists.rb
@@ -0,0 +1,17 @@
+# Required gems
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+# Namespace module
+module TopArtists
+  
+end
+
+# Required files
+require_relative "./top_artists/version"
+# require_relative "./top_artists/scraper"
+require_relative "./top_artists/cli"
+require_relative "./top_artists/artists"
+# require_relative "./top_artists/artistSong"
