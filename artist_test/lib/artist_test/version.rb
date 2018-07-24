deea8b978b94519fdfd48b74313ca8197674b6ca
Joana Palero <joana.palero24@gmail.com>
2018-07-24 22:59:47 +0000

    automatically backed up by learn

--- artist_test/lib/artist_test/version.rb
+++
@@ -1,3 +0,0 @@
-module ArtistTest
-  VERSION = "0.1.0"
-end
--- artist_test/spec/artist_test_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe ArtistTest do
-  it "has a version number" do
-    expect(ArtistTest::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
